d6d668e8479f7a65c4d167379be8b18114d7511c
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-05-22 12:43:56 -0700

    simple impl query results parsers

--- R/get.R
+++ R/get.R
@@ -9,31 +9,33 @@
 #' @param path character string (optional) specifying the path to write query
 #' results to.
 #' @export
-im_get <- function(im, as = "tab", path) {
+im_get <- function(im, as = "tab", path = NULL) {
 
 
   im <- parse_for_service(im, as)
 
-  httr::GET(im$url, path = im$service, query = im["query"], httr::accept("*/*"))
+  r <- httr::GET(im$url, path = im$service, query = im["query"], httr::accept("*/*"))
+
+  parse_results(r, as, path)
 }
 
 #' @rdname im_get
 #' @inheritParams im_get
 #' @export
-im_get_gff3 <- function(im, path) {
+im_get_gff3 <- function(im, path = NULL) {
   im_get(im, as = "gff3", path)
 }
 
 #' @rdname im_get
 #' @inheritParams im_get
 #' @export
-im_get_fasta <- function(im, path) {
+im_get_fasta <- function(im, path = NULL) {
   im_get(im, as = "fasta", path)
 }
 
 #' @rdname im_get
 #' @inheritParams im_get
 #' @export
-im_get_bed <- function(im, path) {
+im_get_bed <- function(im, path = NULL) {
   im_get(im, as = "bed", path)
 }
---
+++ R/parse_results.R
@@ -0,0 +1,54 @@
+#' parse results of a request
+#' @param r request object
+#' @param path character string (optional) specifying the path to write query
+#' results to.
+#' @keywords internal
+parse_results <- function(r, as, path) {
+  # to write to file
+  if(!is.null(path)) {
+    httr::content(r, "raw") %>%
+      writeBin(path)
+  }
+
+  # to parse
+  switch(as,
+         "gff3" = parse_gff3_results(r),
+         "fasta" = parse_fasta_results(r),
+         "bed" = parse_bed_results(r))
+}
+
+#' parse gff3 results into data.frame
+#' @param r request object
+#' @keywords internal
+parse_gff3_results <- function(r) {
+  out <- httr::content(r,"text") %>%
+    data.table::fread(skip = "##", header=FALSE)
+  data.table::setattr(out,"class","data.frame")
+  data.table::setnames(out,c("seqname", "source", "feature", "start", "end",
+                             "score", "strand", "frame", "group"))
+  out
+}
+
+#' parse fasta results into named list
+#' @param r request object
+#' @keywords internal
+parse_fasta_results <- function(r) {
+  warning("no parsing available for fasta yet. \n returning text.")
+  httr::content(r,"text")
+}
+
+#' parse bed results into data.frame
+#' @param r request object
+#' @keywords internal
+parse_bed_results <- function(r) {
+  out <- httr::content(r,"text") %>%
+    data.table::fread(skip = "track", header=FALSE)
+
+  bed_cols <- c("chrom", "chromStart", "chromEnd", "name", "score", "strand",
+                "thickStart", "thickEnd", "itemRgb", "blockCount", "blockSizes",
+                "blockStarts")
+  data.table::setnames(out,bed_cols[1:ncol(out)])
+  data.table::setattr(out,"class","data.frame")
+  out
+}
+
